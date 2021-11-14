9a1285bce590856ecf7162633ccb53c3efaa292b
Ung Wei Liang <liang2010@hotmail.my>
2021-11-14 16:12:11 +0800

    Fix: cannot open setting fragment

--- app/src/main/java/com/example/android_b19/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/android_b19/ui/setting/SettingFragment.java
@@ -1,8 +1,9 @@
-package com.example.android_b19;
+package com.example.android_b19.ui.setting;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.android_b19.R;
 import com.example.android_b19.ui.login.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
